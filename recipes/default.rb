78bae574155a89747d58e569576c23a5c410f6ac
HAhmed377 <hanad15@hotmail.co.uk>
2018-09-27 16:36:43 +0100

    First commit

---
+++ recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: python
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+apt_update "update_sources" do
+  action :update
+end
+package "python2.7" do
+  action :install
+end
+package "python-pip" do
+  action :install
+end
+package "libncurses5-dev" do
+  action :install
+end
